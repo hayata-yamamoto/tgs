060c24b99e42f31838490ee68165eee66f8fff8f
hayata-yamamoto <hayata.pri.yamamoto@gmail.com>
2018-09-15 16:27:48 +0900

    refactoring

--- reports/u-net.py
+++ reports/u-net.py
@@ -8,7 +8,7 @@ sns.set_style('white')
 
 from sklearn.model_selection import train_test_split
 from skimage.transform import resize
-
+from pathlib import Path
 from keras.preprocessing.image import load_img
 from keras import Model
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
@@ -18,13 +18,6 @@ from keras.utils.vis_utils import plot_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Input, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Dropout
 
-from pathlib import Path
-
-img_size_ori = 101
-img_size_target = 128
-
-PROJECT_ROOT = Path(__file__).absolute().parent.parent
-print(PROJECT_ROOT)
 
 def upsample(img):
     if img_size_ori == img_size_target:
@@ -37,43 +30,10 @@ def downsample(img):
     return resize(img, (img_size_ori, img_size_ori), mode='constant', preserve_range=True)
 
 
-train_df = pd.read_csv(PROJECT_ROOT.joinpath('data/raw/train.csv'), index_col="id", usecols=[0])
-depths_df = pd.read_csv(PROJECT_ROOT.joinpath('data/raw/depths.csv'), index_col="id")
-train_df = train_df.join(depths_df)
-test_df = depths_df[~depths_df.index.isin(train_df.index)]
-
-train_df["images"] = [np.array(load_img(PROJECT_ROOT.joinpath(f'data/raw/train/images/{idx}.png'), grayscale=True)) / 255 for idx in train_df.index]
-train_df["masks"] = [np.array(load_img(PROJECT_ROOT.joinpath(f'data/raw/train/masks/{idx}.png'), grayscale=True)) / 255 for idx in train_df.index]
-
-train_df["coverage"] = train_df.masks.map(np.sum) / pow(img_size_ori, 2)
-
-
 def cov_to_class(val):
     for i in range(0, 11):
         if val * 10 <= i:
             return i
-
-
-train_df["coverage_class"] = train_df.coverage.map(cov_to_class)
-
-ids_train, ids_valid, x_train, x_valid, y_train, y_valid, cov_train, cov_test, depth_train, depth_test = \
-    train_test_split(
-        train_df.index.values,
-        np.array(train_df.images.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1),
-        np.array(train_df.masks.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1),
-        train_df.coverage.values,
-        train_df.z.values,
-        test_size=0.2, stratify=train_df.coverage_class, random_state=1337)
-
-tmp_img = np.zeros((img_size_target, img_size_target), dtype=train_df.images.loc[ids_train[10]].dtype)
-tmp_img[:img_size_ori, :img_size_ori] = train_df.images.loc[ids_train[10]]
-fix, axs = plt.subplots(1, 2, figsize=(15,5))
-axs[0].imshow(tmp_img, cmap="Greys")
-axs[0].set_title("Original image")
-axs[1].imshow(x_train[10].squeeze(), cmap="Greys")
-axs[1].set_title("Scaled image")
-plt.show()
-plt.close()
 
 
 def build_model(input_layer, start_neurons):
@@ -139,79 +99,42 @@ def build_model(input_layer, start_neurons):
     return output_layer
 
 
-input_layer = Input((img_size_target, img_size_target, 1))
-output_layer = build_model(input_layer, 16)
+def RLenc(img, order='F', format=True):
+    """
+    img is binary mask image, shape (r,c)
+    order is down-then-right, i.e. Fortran
+    format determines if the order needs to be preformatted (according to submission rules) or not
 
+    returns run length as an array or string (if format is True)
+    """
+    bytes = img.reshape(img.shape[0] * img.shape[1], order=order)
+    runs = []  ## list of run lengths
+    r = 0  ## the current run length
+    pos = 1  ## count starts from 1 per WK
+    for c in bytes:
+        if (c == 0):
+            if r != 0:
+                runs.append((pos, r))
+                pos += r
+                r = 0
+            pos += 1
+        else:
+            r += 1
 
-model = Model(input_layer, output_layer)
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.summary()
+    # if last run is unsaved (i.e. data ends with 1)
+    if r != 0:
+        runs.append((pos, r))
+        pos += r
+        r = 0
 
+    if format:
+        z = ''
 
-
-x_train = np.append(x_train, [np.fliplr(x) for x in x_train], axis=0)
-y_train = np.append(y_train, [np.fliplr(x) for x in y_train], axis=0)
-
-fig, axs = plt.subplots(2, 10, figsize=(15,3))
-for i in range(10):
-    axs[0][i].imshow(x_train[i].squeeze(), cmap="Greys")
-    axs[0][i].imshow(y_train[i].squeeze(), cmap="Greens", alpha=0.3)
-    axs[1][i].imshow(x_train[int(len(x_train)/2 + i)].squeeze(), cmap="Greys")
-    axs[1][i].imshow(y_train[int(len(y_train)/2 + i)].squeeze(), cmap="Greens", alpha=0.3)
-fig.suptitle("Top row: original images, bottom row: augmented images")
-plt.show()
-plt.close()
-
-
-early_stopping = EarlyStopping(patience=10, verbose=1)
-model_checkpoint = ModelCheckpoint("./keras.model", save_best_only=True, verbose=1)
-reduce_lr = ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.00001, verbose=1)
-
-epochs = 200
-batch_size = 32
-
-history = model.fit(x_train, y_train,
-                    validation_data=[x_valid, y_valid],
-                    epochs=epochs,
-                    batch_size=batch_size,
-                    callbacks=[early_stopping, model_checkpoint, reduce_lr])
-
-
-fig, (ax_loss, ax_acc) = plt.subplots(1, 2, figsize=(15,5))
-ax_loss.plot(history.epoch, history.history["loss"], label="Train loss")
-ax_loss.plot(history.epoch, history.history["val_loss"], label="Validation loss")
-ax_acc.plot(history.epoch, history.history["acc"], label="Train accuracy")
-ax_acc.plot(history.epoch, history.history["val_acc"], label="Validation accuracy")
-plt.show()
-plt.close()
-
-model = load_model("./keras.model")
-
-
-preds_valid = model.predict(x_valid).reshape(-1, img_size_target, img_size_target)
-preds_valid = np.array([downsample(x) for x in preds_valid])
-y_valid_ori = np.array([train_df.loc[idx].masks for idx in ids_valid])
-
-max_images = 60
-grid_width = 15
-grid_height = int(max_images / grid_width)
-fig, axs = plt.subplots(grid_height, grid_width, figsize=(grid_width, grid_height))
-for i, idx in enumerate(ids_valid[:max_images]):
-    img = train_df.loc[idx].images
-    mask = train_df.loc[idx].masks
-    pred = preds_valid[i]
-    ax = axs[int(i / grid_width), i % grid_width]
-    ax.imshow(img, cmap="Greys")
-    ax.imshow(mask, alpha=0.3, cmap="Greens")
-    ax.imshow(pred, alpha=0.3, cmap="OrRd")
-    ax.text(1, img_size_ori-1, train_df.loc[idx].z, color="black")
-    ax.text(img_size_ori - 1, 1, round(train_df.loc[idx].coverage, 2), color="black", ha="right", va="top")
-    ax.text(1, 1, train_df.loc[idx].coverage_class, color="black", ha="left", va="top")
-    ax.set_yticklabels([])
-    ax.set_xticklabels([])
-plt.suptitle("Green: salt, Red: prediction. Top-left: coverage class, top-right: salt coverage, bottom-left: depth")
-plt.show()
-plt.close()
+        for rr in runs:
+            z += '{} {} '.format(rr[0], rr[1])
+        return z[:-1]
+    else:
+        return runs
 
 
 def iou_metric(y_true_in, y_pred_in, print_table=False):
@@ -277,6 +200,69 @@ def iou_metric_batch(y_true_in, y_pred_in):
     return np.mean(metric)
 
 
+
+img_size_ori = 101
+img_size_target = 128
+
+PROJECT_ROOT = Path(__file__).absolute().parent.parent
+print(PROJECT_ROOT)
+
+train_df = pd.read_csv(PROJECT_ROOT.joinpath('data/raw/train.csv'), index_col="id", usecols=[0])
+depths_df = pd.read_csv(PROJECT_ROOT.joinpath('data/raw/depths.csv'), index_col="id")
+train_df = train_df.join(depths_df)
+test_df = depths_df[~depths_df.index.isin(train_df.index)]
+
+train_df["images"] = [np.array(load_img(PROJECT_ROOT.joinpath(f'data/raw/train/images/{idx}.png'), grayscale=True)) / 255 for idx in train_df.index]
+train_df["masks"] = [np.array(load_img(PROJECT_ROOT.joinpath(f'data/raw/train/masks/{idx}.png'), grayscale=True)) / 255 for idx in train_df.index]
+train_df["coverage"] = train_df.masks.map(np.sum) / pow(img_size_ori, 2)
+train_df["coverage_class"] = train_df.coverage.map(cov_to_class)
+
+ids_train, ids_valid, x_train, x_valid, y_train, y_valid, cov_train, cov_test, depth_train, depth_test = \
+    train_test_split(
+        train_df.index.values,
+        np.array(train_df.images.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1),
+        np.array(train_df.masks.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1),
+        train_df.coverage.values,
+        train_df.z.values,
+        test_size=0.2, stratify=train_df.coverage_class, random_state=1337)
+
+input_layer = Input((img_size_target, img_size_target, 1))
+output_layer = build_model(input_layer, 16)
+
+model = Model(input_layer, output_layer)
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.summary()
+
+x_train = np.append(x_train, [np.fliplr(x) for x in x_train], axis=0)
+y_train = np.append(y_train, [np.fliplr(x) for x in y_train], axis=0)
+
+fig, axs = plt.subplots(2, 10, figsize=(15,3))
+
+early_stopping = EarlyStopping(patience=10, verbose=1)
+model_checkpoint = ModelCheckpoint("./keras.model", save_best_only=True, verbose=1)
+reduce_lr = ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.00001, verbose=1)
+
+epochs = 200
+batch_size = 32
+
+history = model.fit(x_train, y_train,
+                    validation_data=[x_valid, y_valid],
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    callbacks=[early_stopping, model_checkpoint, reduce_lr])
+
+
+model = load_model("./keras.model")
+
+
+preds_valid = model.predict(x_valid).reshape(-1, img_size_target, img_size_target)
+preds_valid = np.array([downsample(x) for x in preds_valid])
+y_valid_ori = np.array([train_df.loc[idx].masks for idx in ids_valid])
+
+max_images = 60
+grid_width = 15
+grid_height = int(max_images / grid_width)
+
 thresholds = np.linspace(0, 1, 50)
 ious = np.array([iou_metric_batch(y_valid_ori, np.int32(preds_valid > threshold)) for threshold in thresholds])
 
@@ -285,72 +271,9 @@ threshold_best_index = np.argmax(ious[9:-10]) + 9
 iou_best = ious[threshold_best_index]
 threshold_best = thresholds[threshold_best_index]
 
-
-plt.plot(thresholds, ious)
-plt.plot(threshold_best, iou_best, "xr", label="Best threshold")
-plt.xlabel("Threshold")
-plt.ylabel("IoU")
-plt.title("Threshold vs IoU ({}, {})".format(threshold_best, iou_best))
-plt.legend()
-
 max_images = 60
 grid_width = 15
 grid_height = int(max_images / grid_width)
-fig, axs = plt.subplots(grid_height, grid_width, figsize=(grid_width, grid_height))
-for i, idx in enumerate(ids_valid[:max_images]):
-    img = train_df.loc[idx].images
-    mask = train_df.loc[idx].masks
-    pred = preds_valid[i]
-    ax = axs[int(i / grid_width), i % grid_width]
-    ax.imshow(img, cmap="Greys")
-    ax.imshow(mask, alpha=0.3, cmap="Greens")
-    ax.imshow(np.array(np.round(pred > threshold_best), dtype=np.float32), alpha=0.3, cmap="OrRd")
-    ax.text(1, img_size_ori-1, train_df.loc[idx].z, color="black")
-    ax.text(img_size_ori - 1, 1, round(train_df.loc[idx].coverage, 2), color="black", ha="right", va="top")
-    ax.text(1, 1, train_df.loc[idx].coverage_class, color="black", ha="left", va="top")
-    ax.set_yticklabels([])
-    ax.set_xticklabels([])
-plt.suptitle("Green: salt, Red: prediction. Top-left: coverage class, top-right: salt coverage, bottom-left: depth")
-plt.show()
-plt.close()
-
-def RLenc(img, order='F', format=True):
-    """
-    img is binary mask image, shape (r,c)
-    order is down-then-right, i.e. Fortran
-    format determines if the order needs to be preformatted (according to submission rules) or not
-
-    returns run length as an array or string (if format is True)
-    """
-    bytes = img.reshape(img.shape[0] * img.shape[1], order=order)
-    runs = []  ## list of run lengths
-    r = 0  ## the current run length
-    pos = 1  ## count starts from 1 per WK
-    for c in bytes:
-        if (c == 0):
-            if r != 0:
-                runs.append((pos, r))
-                pos += r
-                r = 0
-            pos += 1
-        else:
-            r += 1
-
-    # if last run is unsaved (i.e. data ends with 1)
-    if r != 0:
-        runs.append((pos, r))
-        pos += r
-        r = 0
-
-    if format:
-        z = ''
-
-        for rr in runs:
-            z += '{} {} '.format(rr[0], rr[1])
-        return z[:-1]
-    else:
-        return runs
-
 
 x_test = np.array(
     [upsample(np.array(load_img(PROJECT_ROOT.joinpath(f'data/raw/images/{idx}.png'), grayscale=True))) / 255 for idx in
